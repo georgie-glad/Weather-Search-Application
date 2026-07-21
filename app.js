f4b982d15daa665a80307f71ee02d413b267827c
georgie-glad <georgina.gladwin@outlook.com>
2026-07-21 16:01:50 +0100

    Integrated API into application to enable responsive interface display
    Added functionality to ensure weather description, wind speed, humidity, temperature and city name are updated based on API response.

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 function refreshWeather(response) {
+  // Json response - information about city we are searching for
   let temperatureElement = document.querySelector("#app-temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city; // updates city response based on api response
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description; // updates weather description based on api response
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`; // updates humidity based on api response
+  windElement.innerHTML = `${response.data.wind.speed}km/h`; // updates wind speed based on api response
+  temperatureElement.innerHTML = Math.round(temperature); // updates current temperature based on api response
+}
+
+function formatDate(date) {
+  switch (date.getDay()) {
+    case 0:
+      day = "Sun";
+      break;
+    case 1:
+      day = "Mon";
+      break;
+    case 2:
+      day = "Tue";
+      break;
+    case 3:
+      day = "Weds";
+      break;
+    case 4:
+      day = "Thu";
+      break;
+    case 5:
+      day = "Fri";
+      break;
+    case 6:
+      day = "Sat";
+  }
+
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
+  // make api call and update the interface
   let apiKey = "553c50a943f58673fbta5ob01bd1cb5a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
@@ -21,4 +67,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Lisbon");
+searchCity("London");
